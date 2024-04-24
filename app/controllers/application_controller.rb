c667e11df10a0344a9cb3390b25744039fbe3a6a
shoutaro <850123.a@gmail.com>
2024-04-24 22:20:01 +0900

    application_controller.rbにconfigure_permitted_parametersを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller? 
+  # deviseの処理を行う場合、configure_permitted_parametersを実行して
+
+  private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+    end
 end
