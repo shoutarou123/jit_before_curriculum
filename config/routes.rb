c956364b38b6e7ff6c9c18caabf1a321e195326c
shoutaro <850123.a@gmail.com>
2024-04-25 02:22:46 +0900

    signupの際、認証メール送信するよう実装 letter_opener_webで確認可能 次回user edit画面遷移するよう実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2024_04_24_125624) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
