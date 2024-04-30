d4daff7452c563f95c9bba337bb4b774071c89a8
shoutaro <850123.a@gmail.com>
2024-04-30 21:21:25 +0900

    rails g controller users::dash_boards index 作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_24_125624) do
+ActiveRecord::Schema.define(version: 2024_04_30_083033) do
+
+  create_table "dash_boards", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_dash_boards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +34,5 @@ ActiveRecord::Schema.define(version: 2024_04_24_125624) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dash_boards", "users"
 end
