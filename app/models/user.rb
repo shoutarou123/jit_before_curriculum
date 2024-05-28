944b47f3077ea23132004be4b55ebee79f75b881
shoutaro <850123.a@gmail.com>
2024-05-28 14:54:40 +0900

    サインアップの際、名前を入力するよう変更 サインアップの際、名前入力必要にするためバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_many :dashboards
+  validates :name, presence: true, on: :create
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable  
 end
 
 # database_authenticatable
