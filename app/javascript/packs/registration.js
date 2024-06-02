a60a1e8dd2bfa898025b5fb2c23ad4989e1f191e
shoutaro <850123.a@gmail.com>
2024-06-02 14:50:30 +0900

    confirmに関するjs記述をコメントアウト registrations.edit.htmlの記述をﾃﾞﾌｫﾙﾄに戻す

--- app/javascript/packs/registration.js
+++ app/javascript/packs/registration.js
@@ -1,12 +1,12 @@
-function check() {
-  console.log('check function called');  // 追加
-  if (window.confirm('本当に削除しますか？')) {
-    return true;
-  } else {
-    window.alert('キャンセルされました');
-    return false;
-  }
-}
+// function check() {
+//   console.log('check function called');  // 追加
+//   if (window.confirm('本当に削除しますか？')) {
+//     return true;
+//   } else {
+//     window.alert('キャンセルされました');
+//     return false;
+//   }
+// }
 
-// この関数をグローバルに公開します
-window.check = check;
+// // この関数をグローバルに公開します
+// window.check = check;
