b6073dbdda7444ac92dc0f78de4ade6d7df917dd
shoutaro <850123.a@gmail.com>
2024-06-13 13:10:27 +0900

    rails db:migrate 実施

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_30_083033) do
+ActiveRecord::Schema.define(version: 2024_06_13_040149) do
 
-  create_table "dash_boards", force: :cascade do |t|
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "image"
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_dash_boards_on_user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,5 +37,5 @@ ActiveRecord::Schema.define(version: 2024_04_30_083033) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "dash_boards", "users"
+  add_foreign_key "articles", "users"
 end
