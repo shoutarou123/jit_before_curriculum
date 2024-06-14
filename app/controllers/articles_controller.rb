8a7617aa5b2eb33a3b6897c7229440db673c42df
shoutaro <850123.a@gmail.com>
2024-06-14 20:50:48 +0900

    画像投稿できるように実装途中

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
   
   private
     def article_params
-      params.require(:article).permit(:title, :content)
+      params.require(:article).permit(:title, :content, image: [])
     end
 
 end
